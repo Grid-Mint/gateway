89872e1a035061a2ba9320b9b787d4204a7e7262
Denissius Maximus <deniskramar07@gmail.com>
2026-08-16 00:14:43 +0300

    Add user API endpoints and configuration for user service

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/users", async (IHttpClientFactory httpClientFactory) =>
+{
+    var baseUrl = builder.Configuration["Services:Users:BaseUrl"];
+
+    var client = httpClientFactory.CreateClient();
+    var result = await client.GetStringAsync(baseUrl + "/users");
+    return result;
+});
+
+app.MapGet("/user/{id}", async (IHttpClientFactory httpClientFactory, int id) =>
+{
+    var baseUrl = builder.Configuration["Services:Users:BaseUrl"];
+
+    var client = httpClientFactory.CreateClient();
+    var result = await client.GetStringAsync(baseUrl + "/user/" + id);
+    return result;
+});
 
 app.Run();
