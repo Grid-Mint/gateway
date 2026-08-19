5d6c08c7db36d8c38faac0d4d8492efbe709de26
Denissius Maximus <deniskramar07@gmail.com>
2026-08-19 14:53:29 +0300

    Refactor Serilog configuration for improved logging setup in Program.cs

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -1,16 +1,26 @@
 using Serilog;
+using Serilog.Formatting.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSerilog(((context, configuration) => configuration
-        .WriteTo.Console()
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration
         .WriteTo.File("logs/log-.log", rollingInterval: RollingInterval.Day)
-        .ReadFrom.Configuration(context.Configuration)
-    ));
+        .ReadFrom.Configuration(context.Configuration);
+
+    if (context.HostingEnvironment.IsDevelopment())
+    {
+        configuration.WriteTo.Console();
+    }
+    else
+    {
+        configuration.WriteTo.Console(new JsonFormatter(renderMessage: true));
+    }
+});
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
 
 var app = builder.Build();
 
