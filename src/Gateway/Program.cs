920ff276b50f8f23db9497b57b12647858b2d5c1
Denissius Maximus <deniskramar07@gmail.com>
2026-08-19 14:59:07 +0300

    Refactor Serilog console logging to use custom LokiJsonFormatter for improved log structure

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -1,5 +1,7 @@
 using Serilog;
-using Serilog.Formatting.Json;
+using Serilog.Events;
+using Serilog.Formatting;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +17,7 @@ builder.Host.UseSerilog((context, configuration) =>
     }
     else
     {
-        configuration.WriteTo.Console(new JsonFormatter(renderMessage: true));
+        configuration.WriteTo.Console(new LokiJsonFormatter());
     }
 });
 
@@ -31,3 +33,36 @@ app.MapReverseProxy();
 app.Logger.LogInformation("Gateway started");
 
 app.Run();
+
+// JSON у стилі caddy ({"level":"info","ts":...,"msg":"..."}), щоб Loki
+// коректно визначав рівень логу замість "unk".
+sealed class LokiJsonFormatter : ITextFormatter
+{
+    public void Format(LogEvent logEvent, TextWriter output)
+    {
+        var level = logEvent.Level switch
+        {
+            LogEventLevel.Verbose => "debug",
+            LogEventLevel.Debug => "debug",
+            LogEventLevel.Information => "info",
+            LogEventLevel.Warning => "warn",
+            LogEventLevel.Error => "error",
+            LogEventLevel.Fatal => "fatal",
+            _ => "info",
+        };
+
+        var payload = new Dictionary<string, object?>
+        {
+            ["level"] = level,
+            ["ts"] = logEvent.Timestamp.ToUnixTimeMilliseconds() / 1000.0,
+            ["msg"] = logEvent.RenderMessage(),
+        };
+
+        if (logEvent.Exception is not null)
+        {
+            payload["error"] = logEvent.Exception.ToString();
+        }
+
+        output.WriteLine(JsonSerializer.Serialize(payload));
+    }
+}
